ac9e0bcf43365e313e0275d742b5d692546f2157
Humberto Nacif <gigantedeluz@gmail.com>
2024-11-20 18:18:05 -0300

    Feat: Improving Register serilalizer and view

--- backend/register/serializers/register_serializer.py
+++ backend/register/serializers/register_serializer.py
@@ -1,14 +1,28 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'password_confirm']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password_confirm = attrs.get('password_confirm')
+
+        if password != password_confirm:
+            raise serializers.ValidationError({"password_confirm": "Passwords do not match."})
+
+        return attrs
 
     def create(self, validated_data):
+        validated_data.pop('password_confirm')
+
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
