f6bad0075a44ddafe42040a55c90f50d0fcf9be8
Humberto Nacif <gigantedeluz@gmail.com>
2024-11-28 17:24:13 -0300

    feat: Adding Cursor Pagination

---
+++ backend/newsfeed/tests/test_endless_scroll.py
@@ -0,0 +1,38 @@
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.urls import reverse
+from common.factories import UserFactory
+from newsfeed.factories import RippleetFactory
+
+class RippleetEndlessScrollTest(APITestCase):
+    client = APIClient()
+
+    def setUp(self):
+        self.user = UserFactory(username="test_user")
+        self.client.force_authenticate(user=self.user)
+
+        self.rippleets = RippleetFactory.create_batch(25, author=self.user)
+
+    def test_endless_scroll_pagination(self):
+        url = reverse('rippleet-list')
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(len(response.data['results']), 10)
+        next_url = response.data['next']
+
+        page = 2
+        while next_url:
+            response = self.client.get(next_url)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+            if page <= 2:
+                self.assertEqual(len(response.data['results']), 10)
+            else:
+                self.assertEqual(len(response.data['results']), 5)
+
+            next_url = response.data['next']
+            page += 1
+
+        self.assertIsNone(next_url)
--- backend/newsfeed/tests/test_rippleet_viewset.py
+++ backend/newsfeed/tests/test_rippleet_viewset.py
@@ -28,8 +28,9 @@ class RippleetViewSetTest(APITestCase):
     def test_get_rippleet_list(self):
         response = self.client.get(self.rippleet_list_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["content"], self.rippleet.content)
+        self.assertIn('results', response.data)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]["content"], self.rippleet.content)
 
     def test_update_rippleet(self):
         updated_content = {
