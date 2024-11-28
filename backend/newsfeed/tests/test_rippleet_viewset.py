9f926408c8cc5a593edc9452476bfdebe70638ed
Humberto Nacif <gigantedeluz@gmail.com>
2024-11-28 18:17:13 -0300

    test: Writing tests for Like and Delete permissions functionalities

--- backend/newsfeed/tests/test_rippleet_viewset.py
+++ backend/newsfeed/tests/test_rippleet_viewset.py
@@ -15,6 +15,7 @@ class RippleetViewSetTest(APITestCase):
         self.rippleet = RippleetFactory(author=self.user)
         self.rippleet_url = reverse('rippleet-detail', kwargs={'pk': self.rippleet.id})
         self.rippleet_list_url = reverse('rippleet-list')
+        self.like_url = reverse('rippleet-like', kwargs={'pk': self.rippleet.id})
 
     def test_create_rippleet(self):
         data = {
@@ -46,7 +47,28 @@ class RippleetViewSetTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Rippleet.objects.count(), 0)
 
+    def test_delete_rippleet_permission(self):
+        another_user = UserFactory()
+        self.client.force_authenticate(user=another_user)
+        
+        response = self.client.delete(self.rippleet_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Rippleet.objects.count(), 1)
+
     def test_get_rippleet_detail(self):
         response = self.client.get(self.rippleet_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["content"], self.rippleet.content)
+
+    def test_like_rippleet(self):
+        response = self.client.post(self.like_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["message"], "You have liked this rippleet.")
+        self.rippleet.refresh_from_db()
+        self.assertEqual(self.rippleet.likes.count(), 1)
+
+        response = self.client.post(self.like_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["message"], "You have unliked this rippleet.")
+        self.rippleet.refresh_from_db()
+        self.assertEqual(self.rippleet.likes.count(), 0)
