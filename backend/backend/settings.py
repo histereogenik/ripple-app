094d63cce476fba5c125bf0a518af515494ec667
Humberto Nacif <gigantedeluz@gmail.com>
2024-12-27 13:39:19 -0300

    config: Adding cors host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,6 +151,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://ripple-app-ikz8.onrender.com",
 ]
 
 CORS_ALLOW_HEADERS = [
